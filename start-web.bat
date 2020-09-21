31eac46f49c72567b04bd23cc0388ae547e95e56
ani <48226712+ani9001@users.noreply.github.com>
2020-09-21 18:00:20 +0900

    Create start-web.bat

---
+++ start-web.bat
@@ -0,0 +1,21 @@
+@echo off
+cd /d "%~dp0"
+
+(echo version: '3.8'
+ echo services:
+ echo  web:
+ echo   image: httpd:alpine
+ echo   ports:
+ echo    - "8080:80"
+ echo   volumes:
+ echo    - C:\Users\xxx\Downloads:/usr/local/apache2/htdocs/
+)>docker-compose.yml
+
+docker-compose up -d
+
+echo 起動しました. 終了するにはなにかキーを押してください.
+pause >nul
+
+docker-compose stop
+docker-compose down --rmi all
+del /q docker-compose.yml >nul
