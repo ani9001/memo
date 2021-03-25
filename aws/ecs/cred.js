613f094bb62deaa35c4323f044cba9a50344ffa8
ani <48226712+ani9001@users.noreply.github.com>
2021-03-25 19:50:35 +0900

    Create cred.js

---
+++ aws/ecs/cred.js
@@ -0,0 +1,10 @@
+// common
+AWS.config.update({
+ accessKeyId:    'xxxxxxxxxxx',
+ secretAccessKey:'xxxxxxxxxxxxxxxxxxxxxxxxx',
+ region:         'ap-northeast-1'
+});
+
+// ecs
+serviceName = "xxxx-selenium"
+clusterName = "xxxx-selenium"
