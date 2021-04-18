1be0cf1bee242ba98c4e5a157f4679c7c36a1f3e
ani <48226712+ani9001@users.noreply.github.com>
2021-04-18 15:41:31 +0900

    Create fanbox-sh-diff.sh

---
+++ fanbox/fcm-diff/fanbox-sh-diff.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pushInfoCsv=$(curl -s "https://api.fanbox.cc/post.listCreator?creatorId=${creatorId}&limit=1" -H 'Origin: https://www.fanbox.cc' | jq -r '.body.items[] | [ .user.iconUrl, .user.name, .title ] | @csv' | sed 's/"//g')
+pushInfo=(${pushInfoCsv//,/ })
+if [ ${#pushInfo[@]} -ge 3 ]; then
+ pushImageUrl=${pushInfo[0]}
+ pushBody="${pushInfo[1]}/${pushInfo[2]}"
+ puchClickAction="https://${creatorId}.fanbox.cc/"
+fi
+
+curl -X POST -H "Authorization: key=${fcmKey}" -H Content-Type:"application/json" -d '{
+ "registration_ids": [ '${registrationIds}' ],
+ "notification": {
+  "title": "'${fcmTitle}'",
+  "body": "'${pushBody}'",
+  "icon": "'${pushImageUrl}'",
+  "click_action": "'${puchClickAction}'"
+ }
+}' https://fcm.googleapis.com/fcm/send
