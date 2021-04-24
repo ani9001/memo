535c311457f5b579e0db8771e718a52881089707
ani <48226712+ani9001@users.noreply.github.com>
2021-04-24 10:25:52 +0900

    Create tweet.py

---
+++ bot/tweet.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('/usr/local/lib/python3.7/site-packages')
+
+import json
+import tweepy
+
+import tweet_bot_token as token
+
+auth = tweepy.OAuthHandler(token.consumer_key, token.consumer_secret)
+auth.set_access_token(token.access_token, token.access_token_secret)
+api = tweepy.API(auth)
+
+if __name__ == '__main__':
+ args = sys.argv
+ if len(args) == 2:
+  api.update_status(args[1])
